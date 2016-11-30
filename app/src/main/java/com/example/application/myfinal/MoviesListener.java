cee41d32e6f3759228f3bc5b92718c8bddc35bf3
OsamaWagdy <osamawagdy32@gmail.com>
2016-12-01 00:06:09 +0200

    second commit

---
+++ app/src/main/java/com/example/application/myfinal/MoviesListener.java
@@ -0,0 +1,8 @@
+package com.example.application.myfinal;
+
+
+
+public interface MoviesListener {
+
+     void setselectedmovie(String movieposter,String movieover,String movietitle,String moviedate,String movievote,String movieid);
+}
